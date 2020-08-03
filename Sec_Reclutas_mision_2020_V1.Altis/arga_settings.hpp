374e4da3c83b275e7dc6eda62e2524b87f9591de
FrancoCarlino <carlinoafranco@gmail.com>
2020-08-03 12:58:57 -0300

    Campo Rec Mision

---
+++ Sec_Reclutas_mision_2020_V1.Altis/arga_settings.hpp
@@ -0,0 +1,79 @@
+﻿#define NAME Campo para Reclutas ArgA
+#define VERSION v1.4
+#define IMAGE "imgs\portada.jpg"
+#define DESCRIPTION "Sección Reclutas"
+
+author = "Axel, Shaggy";
+IMG_LOGO = "imgs\logo.paa";
+COLOR_AUTOR = "#76a8f7";
+ES_CAMPANIA = 0;
+NOMBRE_CAMPANIA = "";
+INTRO = 2;
+MAX_DIST_VISION = 5000;
+DESACTIVAR_EQUIPAMIENTO_PERSONALIZADO = 0;
+CURATOR_WHITELIST = ["|ArgA|Axel|", "|ArgA|Shaggy|", "|Sgt", "|Cpt", "|Tte", "|Cmd"];
+DEBUG = 1;                                  // Habilita el modo debug
+ENABLE_LOG_SYSTEM = 1;                      // Habilita el sistema de log
+ENABLE_LOG_SYSTEM_ONLY_DEDICATED = 1;       // Habilita el sistema de log sólo si el servidor es dedicado (requiere ENABLE_LOG_SYSTEM = 1)
+
+//EQUIPAMIENTO
+// Camuflajes disponibles:          Tipos disponibles:
+//      - Bosque                        - Multicam 
+//      - Desierto                      - Marpat
+//      - Selva
+//
+//      - Personalizado (se debe ingresar mas abajo la clase del uniforme, chaleco, casco y mochila)
+//      - No editar (no se le modificara ninguna de las 4 cosas al jugador, solo arma y equipo interno)
+//
+//        Las opciones Personalizada y No editar pueden llevar a comportamientos inesperados.
+//        Por lo que el editor queda a cargo de checkear y revisar el correcto funcionamiento.
+
+CAMUFLAJE = ["Bosque", "Multicam"];
+GAFAS_INICIALES = "";
+UNIFORME_PERSONALIZADO = "";
+CASCO_PERSONALIZADO = "";
+CHALECO_PERSONALIZADO = "";
+MOCHILA_PERSONALIZADO = "";
+MOCHILA_CHICA_PERSONALIZADO = "";
+
+//BRIEFING
+// Cada elemento del array es un parrafo
+// Ejemplo: ["Primer parrafo", "Segundo parrafo"]
+CARACTER = "";
+HORARIO = "";
+FECHA = "";
+CLIMA = "";
+UBICACION = "";
+SITUACION = [""];               
+SITUACION_HISTORICA = [""];     
+EXTRACTO_INTELIGENCIA = [""];
+OBJETIVOS = [""];
+RECURSOS = [""];
+ENEMIGOS = [""];
+////////////////////////////////////////////////////////////////////////////////
+
+//IA - CONFIGURACION
+DESACTIVAR_IA_DE_GRUPO = 1; // 0 Permite la IA del Grupo de los jugadores, 1 la desactiva.
+DESACTIVAR_TODO_BLUFOR = 0; // 0 Permite la IA del Blufor, 1 la desactiva.
+DIFICULTAD_IA_PERSONALIZADA = 1;
+AIMING_ACCURACY = 0.01; //precision de apuntado
+AIMING_SHAKE = 0.99; 	//dispersión de apuntado
+AIMING_SPEED = 0.1; 	//velocidad de apuntado
+SPOT_DISTANCE = 0.3; 	//distancia de avistamiento
+SPOT_TIME = 0.5; 		//tiempo de avistamiento
+COURAGE = 0.25; 		//coraje
+RELOAD_SPEED = 0.25; 	//velocidad de recarga
+COMMANDING = 0.75; 		//liderazgo
+////////////////////////////////////////////////////////////////////////////////
+
+/* Modificar desde los define arriba de todo, no tocar directamente */
+onLoadName = NAME;	                        // Nombre de carga
+briefingName = NAME VERSION;                // Nombre en briefing
+onLoadMission = DESCRIPTION;                // Pie de carga
+overviewText = DESCRIPTION;                 // Pie en seleccion de mision
+overviewPicture = IMAGE;                    // Imagen en seleccion de mision
+loadScreen = IMAGE;                         // Imagen en carga
+
+/*******************************************************************************
+                          Realizado por |ArgA|MandI
+*******************************************************************************/
