55eb7968daba3423060b87d6a5e67dd6cf8397f9
Roke <roquehoraciomansilla@gmail.com>
2021-08-14 17:14:28 -0300

    actualizacion camo relutas

--- escuela_reclutas.hebontes/arga_settings.hpp
+++ escuela_reclutas.hebontes/arga_settings.hpp
@@ -3,7 +3,7 @@
 *******************************************************************************/
 
 #define NAME "Escuela de reclutas ArgA"
-#define COMPLETE_NAME "escuela de reclutas ArgA V1.6"
+#define COMPLETE_NAME "escuela de reclutas ArgA V1.8"
 #define IMAGE "imgs\bandera2.jpg"
 #define DESCRIPTION "Escuela Militar Oscar Poltronieri Seccion Reclutas"
 
